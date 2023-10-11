924145b69fdb3da467d376077cb55814db5cee00
mje0002 <mje0002@gmail.com>
2023-10-10 22:09:40 -0400

    In the beginning

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,54 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
+/*
+  This file is used to do things with in the tab/page that you are after is has loaded.
+  This means things like window linsteners and so on.
+ */
+
+/*
+I want to be able to console log the typed input from an editable content field.
+
+1. listen for on focus events
+2. if focsed event is on an editable content field - console.log the field and I have captured!
+3. listen to typing / input on the field - loog content on PAUSE ...
+*/
+
+const detectFocus: any = () => {
+  console.log(`Focus has occured! Element in question is`, document.activeElement);
+}
+
+window.addEventListener('focus', detectFocus);
+
+const isEditableElement = (activeElement: Element) => {
+
+  if (!activeElement) {
+    return false;
   }
-});
+
+  const tagName = activeElement.tagName.toUpperCase();
+  const isContentEditable = activeElement.getAttribute('contenteditable');
+
+  if (tagName === 'TEXTAREA' || tagName === 'INPUT' || isContentEditable) {
+    return true;
+  }
+  return false;
+}
+
+// const listenActiveElement = (callback: any) => {
+//   // Initial check
+//   let lastActiveElement = document.activeElement;
+//   // Check if element is textarea or input
+//   if (isTextareaOrInput(lastActiveElement)) {
+//     callback(lastActiveElement);
+//   }
+
+//   // Handle if focus changes
+//   const detectFocus = () => {
+//     if (lastActiveElement !== document.activeElement) {
+//       lastActiveElement = document.activeElement;
+//       if (isTextareaOrInput(lastActiveElement)) {
+//         callback(lastActiveElement);
+//       }
+//     }
+//   };
+
+//   window.addEventListener("focus", detectFocus, true);
+// };
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,50 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 
 const Popup = () => {
-  const [count, setCount] = useState(0);
-  const [currentURL, setCurrentURL] = useState<string>();
-
-  useEffect(() => {
-    chrome.action.setBadgeText({ text: count.toString() });
-  }, [count]);
-
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      setCurrentURL(tabs[0].url);
-    });
-  }, []);
-
-  const changeBackground = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      const tab = tabs[0];
-      if (tab.id) {
-        chrome.tabs.sendMessage(
-          tab.id,
-          {
-            color: "#555555",
-          },
-          (msg) => {
-            console.log("result message:", msg);
-          }
-        );
-      }
-    });
-  };
-
   return (
     <>
-      <ul style={{ minWidth: "700px" }}>
-        <li>Current URL: {currentURL}</li>
-        <li>Current Time: {new Date().toLocaleTimeString()}</li>
-      </ul>
-      <button
-        onClick={() => setCount(count + 1)}
-        style={{ marginRight: "5px" }}
-      >
-        count up
-      </button>
-      <button onClick={changeBackground}>change background</button>
+      <div style={{ minWidth: "350px" }}>
+        <img src="./icons8-ai-96.png"></img>
+        <span>Under Construction</span>
+      </div>
     </>
   );
 };
