7ce97ee364a35eaeb608affdbc0221f2f097396b
mje0002 <mje0002@gmail.com>
2023-10-12 21:01:38 -0400

    getting it reading from the inputs

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 
 const Popup = () => {
+  const [text, setText] = useState('');
+
+  chrome.storage.sync.get(['inputtext'], function (data) {
+    setText(data.inputtext ?? '');
+  });
+
   return (
     <>
       <div style={{ minWidth: "350px" }}>
         <img src="./icons8-ai-96.png"></img>
-        <span>Under Construction</span>
+        <span>{text}</span>
       </div>
     </>
   );
